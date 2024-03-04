04d6b9c45c359dec78cb9d85dd44c9013d595190
MohaAgha9 <159828795+MohaAgha9@users.noreply.github.com>
2024-03-05 00:02:40 +0200

    Delete hello.py
    @MohaAgha9

--- hello.py
+++
@@ -1,8 +0,0 @@
-print("welcome to this project")
-print("Welcome to Internet protocol project")
-print("this will be version 1.2")
-print("new version 1")
-print(2+2)
-
-name = "Md.Shawkot Hossain"
-print(f"my name is {name}")
